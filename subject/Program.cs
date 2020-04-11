4de7214f9aa515ccdea4528a307cbd714ad4f1b5
Denys Mentiei <endden@gmail.com>
2020-04-11 18:37:38 +0300

    Added dummy C# subject.

---
+++ subject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace subject
+{
+    internal sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, sailor!");
+        }
+    }
+}
