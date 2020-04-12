5705665a9acbf3acb7fa443a34c5b6eb8cd57561
Denys Mentiei <endden@gmail.com>
2020-04-12 18:25:44 +0300

    aps: Module & TypeRef table parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+#![allow(unused_imports)]
+#![allow(unused_assignments)]
+#![allow(unused_mut)]
+
 mod utils;
 mod buf;
 
@@ -72,44 +76,44 @@ const METADATA_STREAM_NAME_MAX_LEN: usize = 32;
 
 // Taken from ECMA II.22
 
-const METADATA_MODULE:                 u8 = 0x00;
-const METADATA_TYPEREF:                u8 = 0x01;
-const METADATA_TYPEDEF:                u8 = 0x02;
-const METADATA_FIELD:                  u8 = 0x04;
-const METADATA_METHODDEF:              u8 = 0x06;
-const METADATA_PARAM:                  u8 = 0x08;
-const METADATA_INTERFACEIMPL:          u8 = 0x09;
-const METADATA_MEMBERREF:              u8 = 0x0A;
-const METADATA_CONSTANT:               u8 = 0x0B;
-const METADATA_CUSTOMATTRIBUTE:        u8 = 0x0C;
-const METADATA_FIELDMARSHAL:           u8 = 0x0D;
-const METADATA_DECLSECURITY:           u8 = 0x0E;
-const METADATA_CLASSLAYOUT:            u8 = 0x0F;
-const METADATA_FIELDLAYOUT:            u8 = 0x10;
-const METADATA_STANDALONESIG:          u8 = 0x11;
-const METADATA_EVENTMAP:               u8 = 0x12;
-const METADATA_EVENT:                  u8 = 0x14;
-const METADATA_PROPERTYMAP:            u8 = 0x15;
-const METADATA_PROPERTY:               u8 = 0x17;
-const METADATA_METHODSEMANTICS:        u8 = 0x18;
-const METADATA_METHODIMPL:             u8 = 0x19;
-const METADATA_MODULEREF:              u8 = 0x1A;
-const METADATA_TYPESPEC:               u8 = 0x1B;
-const METADATA_IMPLMAP:                u8 = 0x1C;
-const METADATA_FIELDRVA:               u8 = 0x1D;
-const METADATA_ASSEMBLY:               u8 = 0x20;
-const METADATA_ASSEMBLYPROCESSOR:      u8 = 0x21;
-const METADATA_ASSEMBLYOS:             u8 = 0x22;
-const METADATA_ASSEMBLYREF:            u8 = 0x23;
-const METADATA_ASSEMBLYREFPROCESSOR:   u8 = 0x24;
-const METADATA_ASSEMBLYREFOS:          u8 = 0x25;
-const METADATA_FILE:                   u8 = 0x26;
-const METADATA_EXPORTEDTYPE:           u8 = 0x27;
-const METADATA_MANIFESTRESOURCE:       u8 = 0x28;
-const METADATA_NESTEDCLASS:            u8 = 0x29;
-const METADATA_GENERICPARAM:           u8 = 0x2A;
-const METADATA_METHODSPEC:             u8 = 0x2B;
-const METADATA_GENERICPARAMCONSTRAINT: u8 = 0x2C;
+const METADATA_MODULE:                 usize = 0x00;
+const METADATA_TYPEREF:                usize = 0x01;
+const METADATA_TYPEDEF:                usize = 0x02;
+const METADATA_FIELD:                  usize = 0x04;
+const METADATA_METHODDEF:              usize = 0x06;
+const METADATA_PARAM:                  usize = 0x08;
+const METADATA_INTERFACEIMPL:          usize = 0x09;
+const METADATA_MEMBERREF:              usize = 0x0A;
+const METADATA_CONSTANT:               usize = 0x0B;
+const METADATA_CUSTOMATTRIBUTE:        usize = 0x0C;
+const METADATA_FIELDMARSHAL:           usize = 0x0D;
+const METADATA_DECLSECURITY:           usize = 0x0E;
+const METADATA_CLASSLAYOUT:            usize = 0x0F;
+const METADATA_FIELDLAYOUT:            usize = 0x10;
+const METADATA_STANDALONESIG:          usize = 0x11;
+const METADATA_EVENTMAP:               usize = 0x12;
+const METADATA_EVENT:                  usize = 0x14;
+const METADATA_PROPERTYMAP:            usize = 0x15;
+const METADATA_PROPERTY:               usize = 0x17;
+const METADATA_METHODSEMANTICS:        usize = 0x18;
+const METADATA_METHODIMPL:             usize = 0x19;
+const METADATA_MODULEREF:              usize = 0x1A;
+const METADATA_TYPESPEC:               usize = 0x1B;
+const METADATA_IMPLMAP:                usize = 0x1C;
+const METADATA_FIELDRVA:               usize = 0x1D;
+const METADATA_ASSEMBLY:               usize = 0x20;
+const METADATA_ASSEMBLYPROCESSOR:      usize = 0x21;
+const METADATA_ASSEMBLYOS:             usize = 0x22;
+const METADATA_ASSEMBLYREF:            usize = 0x23;
+const METADATA_ASSEMBLYREFPROCESSOR:   usize = 0x24;
+const METADATA_ASSEMBLYREFOS:          usize = 0x25;
+const METADATA_FILE:                   usize = 0x26;
+const METADATA_EXPORTEDTYPE:           usize = 0x27;
+const METADATA_MANIFESTRESOURCE:       usize = 0x28;
+const METADATA_NESTEDCLASS:            usize = 0x29;
+const METADATA_GENERICPARAM:           usize = 0x2A;
+const METADATA_METHODSPEC:             usize = 0x2B;
+const METADATA_GENERICPARAMCONSTRAINT: usize = 0x2C;
 
 fn main() -> Result<()> {	
 	println!("Hello, sailor!");
@@ -327,8 +331,18 @@ fn main() -> Result<()> {
 	Ok(())
 }
 
+macro_rules! max {
+	($x:expr) => ($x);
+	($x:expr, $($xs:expr),+) => {
+		{
+			use std::cmp::max;
+			max($x, max!($($xs),+))
+		}
+	};
+}
+
+// II.24.2.6
 fn read_logical_tables(data: &[u8]) -> Result<()> {
-	// TODO(dmi): @incomplete Handle this?
 	// The HeapSizes field is a bitvector that encodes the width of
 	// indexes into the various heaps. If bit 0 is set, indexes into
 	// the #String heap are 4 bytes wide; if bit 1 is set, indexes
@@ -338,7 +352,13 @@ fn read_logical_tables(data: &[u8]) -> Result<()> {
 	// that heap are 2 bytes wide.
 	let heap_sizes = data[6..].read_u8()?;
 	println!("Heap sizes: {:#010b}", heap_sizes);
-
+	let si_size: usize = if heap_sizes & 0x01 == 0 { 2 } else { 4 };
+	let gi_size: usize = if heap_sizes & 0x02 == 0 { 2 } else { 4 };
+	let bi_size: usize = if heap_sizes & 0x04 == 0 { 2 } else { 4 };
+	println!("String index size: {} byte(s).", si_size);
+	println!("Guid index size: {} byte(s).", gi_size);
+	println!("Blob index size: {} byte(s).", bi_size);
+	
 	// The Valid field is a 64-bit bitvector that has a specific bit
 	// set for each table that is stored in the stream; the mapping of
 	// tables to indexes is given at the start of II.22.
@@ -346,12 +366,169 @@ fn read_logical_tables(data: &[u8]) -> Result<()> {
 	let n = valid_mask.count_ones() as usize;
 	println!("Valid mask: {:#066b} -> {} table(s).", valid_mask, n);
 
-	let rows = &data[24..24 + n * 4];
-	let tables = &data[24 + n * 4..];
-	
-	dump(tables, 64);
+	let row_lens = &data[24..24 + n * 4];
+	let tables   = &data[24 + n * 4..];
 
-	Ok(())
+	let mut offset: usize = 0;
+	let mut t: usize = 0;
+	let data = &tables[offset..];
+
+	let mut table_lens = [0u32; 64];
+	let mut r: usize = 0;
+	for i in 0..table_lens.len() {
+		if (valid_mask >> i) & 1 == 1 {
+			table_lens[i] = row_lens[r..].read_u32()?;
+			r += 4;
+		}
+	}
+	let table_lens = table_lens;
+	
+	// II.24.2.6: The physical representation of a row cell e at a
+	// column with type C is defined as follows: 
+	// - If e is a constant, it is stored using the number of bytes as
+	// specified for its column type C (i.e., a 2-bit mask of type
+	// PropertyAttributes).
+	// - If e is an index into the GUID heap, 'blob', or String heap,
+	// it is stored using the number of bytes as defined in the
+	// HeapSizes field.
+	//- If e is a simple index into a table with index i, it is stored
+	// using 2 bytes if table i has less than 2^16 rows, otherwise it
+	// is stored using 4 bytes.
+	// - If e is a coded index that points into table ti out of n
+	// possible tables t0, ...tn-1, then it is stored as
+	// e << (log n) | tag{ t0, ...tn-1}[ti] using 2 bytes if
+	// the maximum number of rows of tables t0, ...tn-1, is
+	// less than 2^(16 - (log n)), and using 4 bytes otherwise.
+	// The family of finite maps tag {t0, ...tn-1} is defined below.
+	// Note that decoding a physical row requires the inverse of this
+	// mapping. [For example, the Parent column of the Constant table
+	// indexes a row in the Field, Param, or Property tables.  The
+	// actual table is encoded into the low 2 bits of the number,
+	// using the values: 0 => Field, 1 => Param, 2 => Property.The
+	// remaining bits hold the actual row number being indexed.  For
+	// example, a value of 0x321, indexes row number 0xC8 in the Param
+	// table.]
+	
+	// II.22.30
+	if (valid_mask >> METADATA_MODULE) & 1 == 1 {
+		let len  = row_lens[t * 4];
+		println!("Module table with {} item(s).", len);
+
+		for i in 0..len {
+			let generation = data[offset..].read_u16()?;
+			if generation != 0 {
+				Err("Module has invalid generation.")?;
+			}
+			offset += 2;
+
+			let name_si = read_idx(data, offset, si_size)?;
+			offset += si_size;
+			let mvid_gi = read_idx(data, offset, gi_size)?;
+			offset += gi_size;
+
+			// EncId & EncBaseId.
+			offset += gi_size * 2;
+
+			println!("  name index: {:#0x}", name_si);
+			println!("  mvid index: {:#0x}", mvid_gi);
+		}
+		
+		t += 1;
+	}
+
+	// METADATA_TYPEREF
+	// II.22.30
+	if (valid_mask >> METADATA_TYPEREF) & 1 == 1 {
+		let len  = row_lens[t * 4];
+		println!("TypeRef table with {} item(s).", len);
+
+		for i in 0..len {
+			// II.24.2.6:
+			const TAG_MASK: usize = 0b11;
+			const RESOLUTION_SCOPE_MODULE:       usize = 0;
+			const RESOLUTION_SCOPE_MODULE_REF:   usize = 1;
+			const RESOLUTION_SCOPE_ASSEMBLY_REF: usize = 2;
+			const RESOLUTION_SCOPE_TYPE_REF:     usize = 3;
+			
+			let max_len = max!(
+				table_lens[METADATA_MODULE],
+				table_lens[METADATA_MODULEREF],
+				table_lens[METADATA_ASSEMBLYREF],
+				table_lens[METADATA_TYPEREF]) as usize;
+			let size  = if max_len < size_for_big_index(4) { 2 } else { 4 };
+			let shift = log2(TAG_MASK);
+
+			let scope = read_idx(data, offset, size)?;
+			offset += size;
+			let type_name_si = read_idx(data, offset, si_size)?;
+			offset += si_size;
+			let type_namespace_si = read_idx(data, offset, si_size)?;
+			offset += si_size;
+
+			print!("-> ");
+			match scope & TAG_MASK {
+				RESOLUTION_SCOPE_MODULE => print!("Module"),
+				RESOLUTION_SCOPE_MODULE_REF => print!("ModuleRef"),
+				RESOLUTION_SCOPE_ASSEMBLY_REF => print!("AssemblyRef"),
+				RESOLUTION_SCOPE_TYPE_REF => print!("TypeRef"),
+				_ => Err("Invalid ResolutionScope tag.")?,
+			};
+			print!(" {:#0x}, ", scope >> shift);
+			println!("  name index: {:#0x}, namespace index: {:#0x}", type_name_si, type_namespace_si);
+		}
+
+		t += 1;
+	}
+
+	// METADATA_TYPEDEF
+	// METADATA_FIELD
+	// METADATA_METHODDEF
+	// METADATA_PARAM
+	// METADATA_INTERFACEIMPL
+	// METADATA_MEMBERREF
+	// METADATA_CONSTANT
+	// METADATA_CUSTOMATTRIBUTE
+	// METADATA_FIELDMARSHAL
+	// METADATA_DECLSECURITY
+	// METADATA_CLASSLAYOUT
+	// METADATA_FIELDLAYOUT
+	// METADATA_STANDALONESIG
+	// METADATA_EVENTMAP
+	// METADATA_EVENT
+	// METADATA_PROPERTYMAP
+	// METADATA_PROPERTY
+	// METADATA_METHODSEMANTICS
+	// METADATA_METHODIMPL
+	// METADATA_MODULEREF
+	// METADATA_TYPESPEC
+	// METADATA_IMPLMAP
+	// METADATA_FIELDRVA
+	// METADATA_ASSEMBLY
+	// METADATA_ASSEMBLYPROCESSOR
+	// METADATA_ASSEMBLYOS
+	// METADATA_ASSEMBLYREF
+	// METADATA_ASSEMBLYREFPROCESSOR
+	// METADATA_ASSEMBLYREFOS
+	// METADATA_FILE
+	// METADATA_EXPORTEDTYPE
+	// METADATA_MANIFESTRESOURCE
+	// METADATA_NESTEDCLASS
+	// METADATA_GENERICPARAM
+	// METADATA_METHODSPEC
+	// METADATA_GENERICPARAMCONSTRAINT
+
+	return Ok(());
+
+	fn read_idx(data: &[u8], offset: usize, index_size: usize) -> Result<usize> {
+		if index_size == 2 {
+			Ok(data[offset..].read_u16()? as usize)
+		} else {
+			Ok(data[offset..].read_u32()? as usize)
+		}
+	}
+
+	fn size_for_big_index(n: usize) -> usize { 1 << (16 - log2(n)) }
+	fn log2(x: usize) -> usize { if x == 0 { 0 } else { 64usize - x.leading_zeros() as usize } }
 }
 
 fn read_strings(data: &[u8]) -> Result<()> {
